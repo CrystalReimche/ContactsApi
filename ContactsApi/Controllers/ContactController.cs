c6216224cf150b109a67d9ec88880e6a284744d7
Crystal Reimche <36277207+CrystalReimche@users.noreply.github.com>
2023-07-05 21:36:46 -0500

    Add project files.

---
+++ ContactsApi/Controllers/ContactController.cs
@@ -0,0 +1,102 @@
+﻿using ContactsApi.Data;
+using ContactsApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactController : Controller
+    {
+        private readonly ContactsAPIDbContext _dbContext;
+
+        public ContactController(ContactsAPIDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetContacts()
+        {
+            var allContacts = await _dbContext.Contacts.ToListAsync();
+            return Ok(allContacts);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetContact([FromRoute] Guid id)
+        {
+            var contact = await _dbContext.Contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(contact);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(ContactDto addContactDto)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Address = addContactDto.Address,
+                Email = addContactDto.Email,
+                FullName = addContactDto.FullName,
+                Phone = addContactDto.Phone
+            };
+
+            await _dbContext.Contacts.AddAsync(contact);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(contact);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateContact([FromRoute] Guid id, ContactDto updateContactDto)
+        {
+            var contact = await _dbContext.Contacts.FindAsync(id);
+
+            if (contact != null) 
+            {
+                contact.FullName = updateContactDto.FullName;
+                contact.Phone = updateContactDto.Phone;
+                contact.Email = updateContactDto.Email;
+                contact.Address = updateContactDto.Address;
+
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(contact);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteContact([FromRoute] Guid id)
+        {
+            var contact = await _dbContext.Contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _dbContext.Remove(contact);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(contact);
+            }
+        }
+    }
+}
---
+++ ContactsApi/Data/Seed.cs
@@ -0,0 +1,42 @@
+﻿using ContactsApi.Models;
+
+namespace ContactsApi.Data
+{
+    public class Seed
+    {
+        // Seeding InMemoryDatabase
+        public static void SeedAsync(ContactsAPIDbContext dbContext)
+        {
+            if (!dbContext.Contacts.Any())
+            {
+                var contacts = new List<Contact>
+                {
+                    new Contact
+                    {
+                        FullName = "Fermin A. Nussbaum",
+                        Email = "FerminANussbaum@armyspy.com",
+                        Phone = 5852475561,
+                        Address = "2560 Caldwell Road, Rochester, NY 14624"
+                    },
+                    new Contact
+                    {
+                        FullName = "Hayden J. Reiss",
+                        Email = "HaydenJReiss@rhyta.com",
+                        Phone = 3183738123,
+                        Address = "1636 August Lane, Alexandria, LA 71301"
+                    },
+                    new Contact
+                    {
+                        FullName = "Darryl D. Griegoss",
+                        Email = "DarrylDGriego@dayrep.com",
+                        Phone = 7249459064,
+                        Address = "1158 Spruce Drive, Scenery Hill, PA 15360"
+                    }
+                };
+
+                dbContext.Contacts.AddRange(contacts);
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
